b2d0d6eaba948a7ab450cdf1468fbd55da529883
pratik <ppratikrramteke04@gmail.com>
2021-05-23 16:35:09 +0530

    Bean Creation

--- src/main/java/com/SpringBoot/RESTfulServices/Controllers/HelloController.java
+++ src/main/java/com/SpringBoot/RESTfulServices/Controllers/HelloController.java
@@ -3,8 +3,7 @@ package com.SpringBoot.RESTfulServices.Controllers;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.SpringBoot.RESTfulServices.Model.UserDetails;
 
 @RestController
 public class HelloController {
@@ -13,6 +12,12 @@ public class HelloController {
 	 public String hello()
 	 {
 		 return "Hello Pratik";
+	 }
+	 
+	 @GetMapping("/userBean")
+	 public UserDetails userBean()
+	 {
+		 return new UserDetails("Pratik","Ramteke","Chandrapur");
 	 }
 	
 }
---
+++ src/main/java/com/SpringBoot/RESTfulServices/Model/UserDetails.java
@@ -0,0 +1,44 @@
+package com.SpringBoot.RESTfulServices.Model;
+
+import javax.persistence.Entity;
+
+
+public class UserDetails {
+
+	private String firstName;
+	private String lastname;
+	private String city;
+	
+	public UserDetails() {
+		super();
+	}
+	public UserDetails(String firstName, String lastname, String city) {
+		super();
+		this.firstName = firstName;
+		this.lastname = lastname;
+		this.city = city;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastname() {
+		return lastname;
+	}
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	
+	
+	
+	
+	
+}
