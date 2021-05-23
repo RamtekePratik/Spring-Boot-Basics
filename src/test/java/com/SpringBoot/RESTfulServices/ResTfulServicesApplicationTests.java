18af81840c0cc74d675d8ee8544ed1a00bde9875
pratik <ppratikrramteke04@gmail.com>
2021-05-23 15:21:07 +0530

    First Commit

---
+++ src/test/java/com/SpringBoot/RESTfulServices/ResTfulServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SpringBoot.RESTfulServices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResTfulServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
